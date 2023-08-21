8a439fb9de2154af5259ad004a02e2be86bf7c07
tanakit <zeedkhan.tp@gmail.com>
2023-08-22 06:34:40 +0800

    init-security

--- backend/main.py
+++ backend/main.py
@@ -1,77 +1,147 @@
-from fastapi import FastAPI, Depends, HTTPException
-import mysql.connector
-import psycopg2
+from datetime import datetime, timedelta
+from typing import Union
 
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from typing_extensions import Annotated
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "c57c74859f1a2464010e635da3140f5dcad58e7459c4226d33b48f30b4e551cd"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
 
-def connect_to_mysql_and_fetch_data(sql_command):
-    # Database connection parameters
-    postgres_params = {
-        "host": "db_postgres",
-        "user": "username",
-        "password": "password",
-        "database": "main",
-        "port": 5432
-    }
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
-    mysql_params = {
-        "host": "db_mysql",  # This is the service name defined in docker-compose.yml
-        "user": "username",
-        "password": "password",
-        "port": 3306  # MySQL port as defined in docker-compose.yml
-    }
 
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     try:
-        # postgres
-
-        # postgres_connection = psycopg2.connect(**postgres_params)
-        # postgres_cursor = postgres_connection.cursor()
-        # postgres_cursor.execute(sql_command)
-        # postgres_cursor.close()
-
-        # mysql
-        mysql_connection = mysql.connector.connect(**mysql_params)
-        mysql_cursor = mysql_connection.cursor()
-        mysql_cursor.execute(sql_command)
-        mysql_databases = [row[0] for row in mysql_cursor.fetchall()]
-        mysql_cursor.close()
-        mysql_connection.close()
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
 
 
-        return {"mysql_databases": mysql_databases}
-    except Exception as e:
-        return {"error": str(e)}
-
-# Call the function to connect and fetch data
-
-
-@app.get('/')
-async def get_root():
-    return {
-        "message": "Hi"
-    }
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
 
-@app.get('/connect')
-async def get_con():
-    con = connect_to_mysql_and_fetch_data(sql_command="SHOW DATABASES;")
-    return {
-        "message": con,
-    }
+@app.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
-@app.get("/create")
-async def create_db():
-    con = connect_to_mysql_and_fetch_data(sql_command="CREATE DATABASE main_2")
-    return {
-        "message": con
-    }
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user
 
-@app.get("/users")
-async def get_users():
-    con = connect_to_mysql_and_fetch_data(sql_command="SELECT * FROM mysql.user")
-    return {
-        "message": con
-    }
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
